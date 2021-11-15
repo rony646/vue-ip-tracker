e674d655e93e1f643154235f57b0219f0e6ecc4c
rony646 <rony646@users.noreply.github.com>
2021-11-15 15:15:51 -0300

    styling input

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="container">
+    <Header />
+  </div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> !-->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> !-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+* {
+  font-family: 'Roboto', impact, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #111111e1;
+  margin: 0;
+  padding: 0;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+    <header class="header-container">
+        <h1>IP Adress Tracker</h1>
+        <form class="form-container">
+            <input type="text" name="" id="">
+            <button type="submit">
+                <img src="../assets/icon-arrow.svg" alt="arrow icon"/>
+            </button>
+        </form>
+    </header>
+</template>
+
+<script>
+export default {
+   name: 'Header',
+}
+</script>
+
+<style>
+    @import'./header.css';
+</style>

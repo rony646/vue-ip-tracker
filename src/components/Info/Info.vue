5a293ce2f1ce781a1aaa118ebaaac95257b937ed
Rony Silva <rony.silva@grupoboticario.com.br>
2021-11-23 22:27:47 -0300

    styled info card

--- src/components/Info/Info.vue
+++ src/components/Info/Info.vue
@@ -1,6 +1,26 @@
 <template>
     <div class="info-wrapper">
-        <div class="info">INFO AQUI</div>
+        <div class="info-container">
+            <div class="info">
+                <span>IP ADDRESS</span>
+                <strong>192.212.174.101</strong>
+            </div>
+
+            <div class="info">
+                <span>LOCATION</span>
+                <strong>Brooklyn, NY, 1001</strong>
+            </div>
+
+            <div class="info">
+                <span>TIMEZONE</span>
+                <strong>UTC -05:00</strong>
+            </div>
+
+             <div class="info">
+                <span>ISP</span>
+                <strong>SpaceX Starlink</strong>
+            </div>
+        </div>
     </div>
 </template>
 
